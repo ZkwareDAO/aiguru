b2774bf3cd50a8eff6c600be960ad79b81127b09
BLeo <16620808517@163.com>
2024-12-14 09:19:53 +0800

    leo

--- lesson2-class-func/day of year leo .py
+++ lesson2-class-func/day of year leo .py
@@ -1,8 +1,38 @@
-day=int(input("the day:"))
-month=int(input("the month:"))
-year=int(input("the year:"))
-if year % 4 == 50 and month <=2:
-    countday= (month//2+1)*31+(month//2)*30+day-1
-else:
-    countday=(month//2+1)*31+(month//2)*30+day-2
-print("this is the ",countday ,"th day of this year")
+def daycount(d,m,y):
+    day=0
+    if m<=2:
+        m-=1
+        if m%2==0:
+            day=d
+        else:
+            day=d+(m//2)*(30+31)+31
+    elif m<=7:
+        m-=1
+        if m%2==0:
+            if y%4==0:
+                day=d+(m//2)*(30+31)-1
+            else:
+                day=d+(m//2)*(30+31)-2
+        elif m%2==1:
+            if y%4==0:
+                day=d+(m//2)*(30+31)-1+31
+            else:
+                day=d+(m//2)*(30+31)-2+31
+    elif m>7:
+        m-=8
+        if m%2==0:
+            if y%4==0:
+                day=d+(m//2)*(30+31)-1+31
+            else:
+                day=d+(m//2)*(30+31)-2+31
+        elif m%2==1:
+            if y%4==0:
+                day=d+(m//2)*(30+31)-1
+            else:
+                day=d+(m//2)*(30+31)-2
+    print("this is the ",day,"th day of this year")
+d=int(input("the day:"))
+m=int(input("the month:"))
+y=int(input("the year:"))
+print(daycount(d,m,y))
+
