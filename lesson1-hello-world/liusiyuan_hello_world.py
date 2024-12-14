f593321db61613b6fbd7928d6f968afa661fe82d
ylq-ytz <ylq_ytz@uotlook.com>
2024-12-14 14:17:58 +0800

    lsy

---
+++ lesson1-hello-world/liusiyuan_hello_world.py
@@ -0,0 +1,7 @@
+x=int(input("Input nmber of \"Hello World\"s you want:\n"))
+if x<0:
+    print ("Function is curently unavailble.")
+else:
+    while x:
+        print("Hello World!")
+        x-=1
