4d23f6dacd58be00f9bead7f2f03a47df443bad7
RebelCjm <michael.cjm.mc@gmail.com>
2025-02-23 01:10:06 +0800

    1

--- testing-gemini-api/testing-gemini-api-txt.py
+++ testing-gemini-api/testing-gemini-api-txt.py
@@ -1,6 +1,6 @@
 from google import genai#需要在cmd运行"pip install -q -U google-genai"才能用
 
-client = genai.Client(api_key="AIzaSyCHHE2VjvCW4H28b29XbFHZIbZ3O8lklC8")#api_key在“https://aistudio.google.com/app/apikey”免费申请
+client = genai.Client(api_key="(your api key)")#api_key在“https://aistudio.google.com/app/apikey”免费申请
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain what is \"api\""
 )#contents为向ai询问的内容，需要连接vpn
