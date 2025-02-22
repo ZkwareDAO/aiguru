d3314a310bd4949e59c269098315744bdf7ec952
RebelCjm <michael.cjm.mc@gmail.com>
2025-02-23 01:06:19 +0800

    testing api for analyzing images

--- testing-gemini-api/testing-gemini-api-txt.py
+++ testing-gemini-api/testing-gemini-api-txt.py
@@ -4,4 +4,4 @@ client = genai.Client(api_key="AIzaSyCHHE2VjvCW4H28b29XbFHZIbZ3O8lklC8")#api_key
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain what is \"api\""
 )#contents为向ai询问的内容，需要连接vpn
-print(response.text)
+print(response.text)#运行的时候点击右上角那个 三角形 旁边的那个 向下的箭头 ，然后选择运行python文件
